a0d1044d87bb59b0d68087324e96a2a943e2c554
Cheng-DX <2337040896@qq.com>
2022-10-05 22:34:20 +0800

    fix: hahaha

--- scripts/download.sh
+++ scripts/download.sh
@@ -8,22 +8,22 @@ BASE_URL="https://raw.githubusercontent.com/Cheng-DX/config/main/configs/"
 # my readme file
 if [ $1 = "readme" ]
 then
-    curl -o README.md $BASE_URL+="READMETemplate.MD"
+    curl -o README.md $BASE_URL"READMETemplate.MD"
 
 # .gitignore file from create-vite/vue
 elif [ $1 = "gitignore" ]
 then
-    curl -o .gitignore $BASE_URL+=".gitignore"
+    curl -o .gitignore $BASE_URL".gitignore"
 
 # mit license
 elif [ $1 = "mit" ]
 then
-    curl -o LICENSE $BASE_URL+="MITLICENSE"
+    curl -o LICENSE $BASE_URL"MITLICENSE"
 
 # eslint
 elif [ $1 = "eslint" ]
 then
-    curl -o .eslintrc $BASE_URL+=".eslintrc"
+    curl -o .eslintrc $BASE_URL".eslintrc"
     echo "Installing package for you :P"
     ni -D eslint @antfu/eslint-config --prefer-offline
     echo "Done, enjoy :)"
@@ -32,15 +32,14 @@ then
 # uno.config.ts: with presets like attributify, icons, webfonts and my custom rules
 elif [ $1 = "unocss" ]
 then
-    curl -o uno.config.ts $BASE_URL+="uno.config.ts"
+    curl -o uno.config.ts $BASE_URL"uno.config.ts"
     echo "Intalling packages for you :P"
     ni -D unocss @unocss/preset-icons @iconify-json/carbon --prefer-offline
 
 # package.json for starter-ts @see https://github.com/antfu/starter-ts
 elif [ $1 = "ts-packagejson" ]
-then 
-    curl -o package.json $BASE_URL+="ts-package.json"
-
+then
+    curl -o package.json $BASE_URL"ts-package.json"
 else
     echo "Invalid param"
 fi
